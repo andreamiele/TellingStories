114f315a46c928fd375b27f762ac11926b776ac9
MurgenB <92163479+MurgenB@users.noreply.github.com>
2022-05-05 19:43:55 +0200

    Indexentete

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php include("entete.php") ?>
 <?php include("nav.php") ?>
-<!DOCTYPE html>
 
-<html lang="fr-fr" xmlns="http://www.w3.org/1999/xhtml">
-
-    <head>
-        <meta charset="utf-8" />
-        <title></title>
-    </head>
-
-    <body>
         <div class="conteneurpage" data-scroll-section>
             <div class="emballage">
                 <div class="accueilsection">
