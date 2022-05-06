44be9e51d2a1f0015b83dc1ac61ab06476393d85
MurgenB <92163479+MurgenB@users.noreply.github.com>
2022-05-06 16:09:20 +0200

    registrationimplementation

--- nav.php
+++ nav.php
@@ -1,5 +1,10 @@
 <?php session_start();
 include('connect.php');
+function secure($user_input)
+{
+    $secure_input=htmlspecialchars($user_input,ENT_QUOTES,'UTF-8',false);
+    return $secure_input;
+}
 ?>
 <div class="white">
     <div class="containeurnav white">
--- registration.php
+++ registration.php
@@ -1,57 +1,71 @@
 <?php
 include('entete.php');
 include('nav.php');
+if(!isset($_POST['Nom']))
+{
 ?>
-<div data-scroll-section>
-  <div class="accueilsection collaborons wf-section">
-    <h2 class="headingcontact">S'inscrire</h2>
-    <div class="center2">
-      <div class="contactblock">
-        <div class="contact">
-          <div class="formblockcentered">
-            <div class="form-block w-form">
-              <?php ?>
-              <form id="wf-form-Contact-Form" name="wf-form-Contact-Form" data-name="Contact Form" method="POST" class="form">
-                <div class="columns-3 w-row">
-                  <div class="columnnopaddingleft w-col w-col-6">
-                    <input type="text" class="contacttextfield w-input" maxlength="256" name="Nom" data-name="Nom" placeholder="Nom" id="Nom" required=""/>
-                    <input type="text" class="contacttextfield w-input" maxlength="256" name="Prénom" data-name="Prénom" placeholder="Prénom" id="Prénom" required=""/>
-                    <input type="email" class="contacttextfield w-input" maxlength="256" name="Email" data-name="Email" placeholder="Email" id="Email" required=""/>
-                    <input type="password" class="contacttextfield w-input" maxlength="256" name="password" data-name="password" placeholder="Mot de passe" id="password" required=""/>
-                    <input type="password" class="contacttextfield w-input" maxlength="256" name="password2" data-name="password2" placeholder="Confirmer le mot de passe" id="password2" required=""/>
+
+  <div data-scroll-section>
+    <div class="accueilsection collaborons wf-section">
+      <h2 class="headingcontact">S'inscrire</h2>
+      <div class="center2">
+        <div class="contactblock">
+          <div class="contact">
+            <div class="formblockcentered">
+              <div class="form-block w-form">
+                <form id="wf-form-Contact-Form" name="wf-form-Contact-Form" data-name="Contact Form" method="POST" class="form">
+                  <div class="columns-3 w-row">
+                    <div class="columnnopaddingleft w-col w-col-6">
+                      <input type="text" class="contacttextfield w-input" maxlength="256" name="Nom" data-name="Nom" placeholder="Nom" id="Nom" required=""/>
+                      <input type="text" class="contacttextfield w-input" maxlength="256" name="Prenom" data-name="Prenom" placeholder="Prénom" id="Prenom" required=""/>
+                      <input type="email" class="contacttextfield w-input" maxlength="256" name="Email" data-name="Email" placeholder="Email" id="Email" required=""/>
+                      <input type="password" class="contacttextfield w-input" maxlength="256" name="password" data-name="password" placeholder="Mot de passe" id="password" required=""/>
+                      <input type="password" class="contacttextfield w-input" maxlength="256" name="password2" data-name="password2" placeholder="Confirmer le mot de passe" id="password2" required=""/>
+                    </div>
+
                   </div>
 
-                </div>
+                  <div class="contactbutton">
+                    <button type='submit' class="bn632-hover bn25">S'inscrire</button>
+                  </div>
+                </form>
 
-                <div class="contactbutton">
-                  <button type='submit' class="bn632-hover bn25">S'inscrire</button>
-                </div>
-              </form>
-
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-
-
-
+<?php
+}
+else
+{ 
+  $Requete="SELECT `login` FROM users;";
+  $response = $BDD->prepare($Requete);
+  $response->execute();
+  $Emails=$response->fetch();
+  if(!in_array($_POST['Email'],$Emails))
+  {
+    $Requete="SELECT MAX(`User_ID`)+1 FROM users;";
+    $response = $BDD->prepare($Requete);
+    $response->execute();
+    $User_number=$response->fetch();
+    $Requete="INSERT INTO users (`User_ID`, Nom, Prenom, `login`, `password`, nbTrophees) VALUES (:ID, :NOM, :PRENOM, :EMAIL, :PASS, 0);";
+    $response = $BDD->prepare($Requete);
+    $response->execute(array("ID"=>$User_number,"NOM"=>secure($_POST['Nom']),"PRENOM"=>secure($_POST['Prenom']),"EMAIL"=>secure($_POST['Email']), "PASS"=>secure($_POST['password'])));
+  }
+  else
+  {?>
+    <h2>Cet Email a déjà été utilisé !</h2>
+    
+  <?php
+  sleep(1);
+  header("Location:registration.php", TRUE, 301);
+  exit();
+  }
+}
+?>
   <div>
-    Ceci est la fin du site.
+      Ceci est la fin du site.
   </div>
-  <footer>
-    Telling stories.
-  </footer>
-
-</div>
-<script src="locomotive-scroll.min.js"></script>
-<script src="js.js"></script>
-<script>
-  (function () {
-    var scroll = new LocomotiveScroll();
-  })();
-</script>
-</body>
-
-</html>
+<?php include('footer.php');?>
