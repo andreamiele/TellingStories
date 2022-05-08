bfb6f3e3d19b31d161d2a472f4641c948d5a58c4
MurgenB <92163479+MurgenB@users.noreply.github.com>
2022-05-08 20:25:42 +0200

    registration form

--- login.php
+++ login.php
@@ -1,18 +1,18 @@
 <?php
 include('entete.php');
 include('nav.php');
-    if(logged($BDD))
+    if(isset($_POST['login']) && isset($_POST['Password']))
     {
-        $_SESSION['connecte']=true;
-        header("Location:index.php", TRUE, 301);
-        exit();
-    }
-    if(isset($_POST['Email']) && isset($_POST['Password']))
-    {
-        $_SESSION['login']=$_POST['Email'];
+        $_SESSION['login']=$_POST['login'];
         $_SESSION['password']=$_POST['Password'];
+        if(logged($BDD))
+        {
+            $_SESSION['connecte']=true;
+            header("Location:index.php", TRUE, 301);
+            exit();
+        }
     }
-    if(!isset($_SESSION['login']) || !isset($_SESSION['password']))
+    if(!logged($BDD))
     {?>
         <div data-scroll-section>
 
@@ -23,10 +23,10 @@ include('nav.php');
                 <div class="contact">
                     <div class="formblockcentered">
                     <div class="form-block w-form">
-                        <form id="wf-form-Contact-Form" name="wf-form-Contact-Form" data-name="Contact Form" method="POST" class="form" action="login.php">
+                        <form id="wf-form-Contact-Form" name="wf-form-Contact-Form" data-name="Contact Form" method="POST" class="form" action="">
                         <div class="columns-3 w-row">
                             <div class="columnnopaddingleft w-col w-col-6">
-                            <input type="email" class="contacttextfield w-input" maxlength="256" name="Email" data-name="Email" placeholder="Email" id="Email" required=""/>
+                            <input type="email" class="contacttextfield w-input" maxlength="256" name="login" data-name="login" placeholder="Email" id="login" required=""/>
 
                             </div>
                             <div class="columnnopadding w-col w-col-6">
