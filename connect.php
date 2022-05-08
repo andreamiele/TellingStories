14539a3788df4a9b9674ec924d0b13c9660c3b34
MurgenB <92163479+MurgenB@users.noreply.github.com>
2022-05-08 23:02:28 +0200

    draft delete session name

--- connect.php
+++ connect.php
@@ -12,11 +12,11 @@ function logged($BDD)
         {
             if($_COOKIE['nb']==1)
             {
-                $Requete="SELECT `Name`, Prenom FROM users WHERE login=:LOGIN";
+                $Requete="SELECT `Nom`, Prenom FROM users WHERE login=:LOGIN";
                 $response = $BDD->prepare($Requete);
                 $response->execute(array("LOGIN" => $_SESSION['login']));
                 $_COOKIE=$response->fetch();
-                $_SESSION['nom']=$_COOKIE['Name'];
+                $_SESSION['nom']=$_COOKIE['Nom'];
                 $_SESSION['prenom']=$_COOKIE['Prenom'];
                 return true;
             }
